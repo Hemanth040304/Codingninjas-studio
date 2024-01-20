f91c3589b6f60a679e6b8410511fbd36c0f62165
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-01-20 23:12:36 +0530

    Easy

---
+++ Check whether K-th bit is set or not.cpp
@@ -0,0 +1,9 @@
+//https://codingninjas.com/studio/problems/check-whether-k-th-bit-is-set-or-not_5026446?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+
+bool isKthBitSet(int n, int k)
+{
+    // Write your code here.
+    if(n&1<<k-1)return true;
+    return false;
+}
