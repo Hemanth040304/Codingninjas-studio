18fc19b18495e22c4d65860f78f590fe30321825
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-01-21 23:51:40 +0530

    Easy

--- Rearrange Array.cpp
+++ Rearrange Array.cpp
@@ -12,3 +12,17 @@ void rearrangeArray(int arr[], int n)
 		arr[i] = copy[copy[i]];
 	}
 }
+
+
+//best approach
+#include <bits/stdc++.h> 
+void rearrangeArray(int arr[], int n)
+{
+	// Write your code here
+	for(int i = 0 ; i < n ; i++){
+		arr[i] = arr[i] + (arr[arr[i]]%n) * n;
+	}
+	for(int i = 0 ; i < n ; i++){
+		arr[i]/=n;
+	}
+}
