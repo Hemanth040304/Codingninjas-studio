a21d85143e697233392c6a8125be9db74d6d2a10
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-01-30 17:42:36 +0530

    Easy

---
+++ Even or Odd.cpp
@@ -0,0 +1,12 @@
+//https://www.codingninjas.com/studio/problems/even-or-odd_7463066?topList=ninjas-sde-sheet&problemListRedirection=true&leftPanelTabValue=PROBLEM
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int n;
+	cin>>n;
+	if(n&1)cout<<"Odd\n";
+	else cout<<"Even\n";
+}
