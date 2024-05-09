1ff4e840060c14e6639a8917afc8e8d002279f87
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-05-09 11:42:44 +0530

    Easy 9th may 2024

---
+++ Reverse the String.cpp
@@ -0,0 +1,10 @@
+//https://www.naukri.com/code360/problem-of-the-day/easy
+// potd 09 - 05 - 2024
+
+#include <bits/stdc++.h> 
+string reverseString(string str)
+{
+	// Write your code here.
+	reverse(str.begin(), str.end());
+	return str;
+}
